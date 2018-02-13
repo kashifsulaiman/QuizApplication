56a95a23ea59f01926367d31d63e2a02e8ee18d8
Mohammad Kashif Sulaiman <m.kqagraphix@gmail.com>
2018-02-14 00:54:47 +0500

    Functionality Implemented

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,3 +1,97 @@
-/**
- * Created by Mohammad Kashif on 2/13/2018.
- */
+import React, { Component } from 'react';
+import {
+    Platform,
+    StyleSheet,
+    Text,
+    View,
+    Button
+} from 'react-native';
+import { RadioButtons, SegmentedControls } from 'react-native-radio-buttons';
+
+export default class Quiz extends Component<Props> {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            quizzes: [],
+            selected : '',
+            options: []
+        };
+
+        this.submit.bind(this);
+    }
+
+    componentWillMount(){
+        this.collectOptions();
+    }
+
+    componentDidUpdate(props){
+        if(this.props.number != props.number){
+            this.setState({selected: ''})
+            this.collectOptions();
+        }
+    }
+
+    collectOptions() {
+        const {quiz} = this.props;
+
+        var options = JSON.parse(JSON.stringify(quiz.incorrect_answers));
+        options.push(quiz.correct_answer);
+        this.setState({options});
+    }
+
+    submit() {
+        const {quiz, number} = this.props;
+        const {selected} = this.state;
+
+        this.props.onSubmit(quiz.correct_answer == selected);
+    }
+
+
+    render() {
+        const {quiz, number} = this.props;
+        const {options, selected} = this.state;
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Question # {number}
+                </Text>
+                <Text>
+                    {quiz.question}
+                </Text>
+
+                <RadioButtons
+                    options={options}
+                    onSelection={ (toSelect) => {this.setState({selected: toSelect})} }
+                    selectedOption={selected}
+                />
+
+                <Button
+                    onPress={this.submit.bind(this)}
+                    disabled={!selected}
+                    title="Submit"
+                    color="#841584"
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
--- src/views/Home.js
+++ src/views/Home.js
@@ -6,32 +6,80 @@ import {
     View,
     Button
 } from 'react-native';
+import Quiz from '../components/Quiz'
 
 export default class Home extends Component<Props> {
     constructor(props){
         super(props);
 
         this.state = {
-
+            quizzes: [],
+            correct: 0,
+            answers: [],
+            currentQuiz: null
         };
-        this.startQuiz.bind(this);
+
+        this.getQuiz();
     }
 
-    startQuiz() {
-
+    getQuiz() {
+        fetch('https://opentdb.com/api.php?amount=10').then((res) => res.json()).then((resp) => {
+            this.setState({quizzes: resp.results});
+        }).catch(() => {
+            this.setState({quizzes: []})
+        })
     }
 
-    render() {
+    renderEnd() {
+        const {correct, quizzes} = this.state;
+
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>
-                    Welcome to Quiz App!
+                    Thanks for the quiz
+                </Text>
+                <Text style={styles.welcome}>
+                    You've scored {correct / quizzes.length * 100} %
                 </Text>
                 <Button
-                    onPress={this.startQuiz}
-                    title="Start the Quiz"
+                    disabled={!quizzes.length}
+                    onPress = {() => this.setState({currentQuiz: 1})}
+                    title="Play again"
                     color="#841584"
                 />
+            </View>
+        )
+    }
+
+
+    onAnswer(isCorrect){
+        const {currentQuiz, quizzes, correct} = this.state;
+        this.setState({correct: isCorrect ? (correct + 1) : correct, currentQuiz: currentQuiz + 1});
+    }
+
+    render() {
+        const {currentQuiz, quizzes} = this.state;
+        return (
+            <View>
+                {currentQuiz ?
+                    currentQuiz == quizzes.length
+                        ?
+                        this.renderEnd()
+                        :
+                        <Quiz quiz={quizzes[currentQuiz - 1]} number={currentQuiz} onSubmit={this.onAnswer.bind(this)}/>
+                    :
+                    <View style={styles.container}>
+                        <Text style={styles.welcome}>
+                            Welcome to Quiz App!
+                        </Text>
+                        <Button
+                            disabled={!quizzes.length}
+                            onPress = {() => this.setState({currentQuiz: 1})}
+                            title="Start Quiz"
+                            color="#841584"
+                        />
+                    </View>
+                }
             </View>
         );
     }
